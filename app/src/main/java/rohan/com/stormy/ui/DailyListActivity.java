0117b5045f7e07e360989b23789ad445883b218f
Deepak Rohan Sekar <deepakrohan@hotmail.com>
2016-03-12 04:30:52 -0500

    OnItemClick Listner

--- app/src/main/java/rohan/com/stormy/ui/DailyListActivity.java
+++ app/src/main/java/rohan/com/stormy/ui/DailyListActivity.java
@@ -1,6 +1,7 @@
 package rohan.com.stormy.ui;
 
 import android.app.ListActivity;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,9 +9,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
+import butterknife.OnClick;
 import rohan.com.stormy.R;
 import rohan.com.stormy.adapters.DayAdapter;
 import rohan.com.stormy.model.Day;
@@ -32,6 +36,18 @@ public class DailyListActivity extends ListActivity {
 
         DayAdapter adapter = new DayAdapter(this,daysList);
         setListAdapter(adapter);
+
+
     }
 
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        String message = daysList.get(position).getSummary();
+        String temperature = daysList.get(position).getMaxTemperature()+"";
+        String dayOfWeek = daysList.get(position).getDayOfWeek();
+
+        String finalS = String.format("On %s it will be %s and today %s",dayOfWeek,temperature,message);
+        Toast.makeText(this,finalS,Toast.LENGTH_SHORT).show();
+    }
 }
